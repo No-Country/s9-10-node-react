b7e5412b60a54342a1ada9354e32414ec30eea40
Diego-Fdez <dieghoxcr7@gmail.com>
2023-07-04 17:00:49 -0600

    add lazy loading

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,37 @@
-import { Route, Link, BrowserRouter, Routes } from "react-router-dom";
-import Logout from "./Components/Logout/Logout";
-import Login from "./Components/Login/Login";
-import Register from "./Components/Register/Register";
+import { Suspense, lazy } from 'react';
+import { Route, Link, BrowserRouter, Routes } from 'react-router-dom';
+
+//add lazy loading to all pages
+const Home = lazy(() => import('./pages/Home/Home'));
+const Logout = lazy(() => import('./pages/Login/Login'));
+const Login = lazy(() => import('./pages/Login/Login'));
+const Register = lazy(() => import('./pages/Register/Register'));
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/logout">Logout</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/logout" element={<Logout />} />
-      </Routes>
-    </BrowserRouter>
+    <Suspense fallback={<div>Loading...</div>}>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/register'>Register</Link>
+            </li>
+            <li>
+              <Link to='/login'>Login</Link>
+            </li>
+            <li>
+              <Link to='/logout'>Logout</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path='/' index element={<Home />} />
+          <Route path='register' element={<Register />} />
+          <Route path='login' element={<Login />} />
+          <Route path='logout' element={<Logout />} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 };
 
