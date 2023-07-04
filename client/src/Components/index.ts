bf3abe94e68a37d60d021d161c8caa49f5b7ea3f
Diego-Fdez <dieghoxcr7@gmail.com>
2023-07-04 17:21:54 -0600

    create barrel

--- client/src/Components/index.ts
+++ client/src/Components/index.ts
@@ -1,1 +1,2 @@
-//solo componentes de orden superior (que se usen en mas de una página)
+//solo componentes de orden superior van aquí (que se usen en mas de una página)
+export { default as NavbarScreen } from './NavbarScreen/NavbarScreen';
