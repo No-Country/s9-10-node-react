4471c38daf350fd9ab1351329d3515e96267d694
Diego-Fdez <dieghoxcr7@gmail.com>
2023-07-04 17:22:11 -0600

    fix styles

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -4,13 +4,13 @@ const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <main className='flex flex-col justify-center items-center h-screen w-full'>
+    <main className='flex flex-col justify-center items-center w-full h-screen overflow-y-hidden'>
       <h1 className='text-4xl font-bold text-center'>
         Bienvenidos al Feedback App
       </h1>
       <button
         className='bg-blue-500 text-white p-2 rounded-md mt-5 px-4 py-2'
-        onClick={() => navigate('/feedback')}
+        onClick={() => navigate('/login')}
       >
         Login
       </button>
