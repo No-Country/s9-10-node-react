440e8a068498a7c0b042bcf5a96909e92dc39fbc
CLOPEZPRO <khrystyan881@hotmail.com>
2023-07-07 16:40:10 -0500

    create new state user and add actions loggin

--- client/src/store/auth.tsx
+++ client/src/store/auth.tsx
@@ -1,18 +1,33 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
-
+import service from "../libs/axios";
 type State = {
   token: string;
+  user: any;
 };
 
 type Actions = {
   setToken: (token: string) => void;
+  getSession:() => void;
+  isLogged: () => boolean;
 };
 
 export const useAuthStore = create(persist<State & Actions>(
     (set) => ({
         token:"",
-        setToken: (newToken: string) => set({ token: newToken })
+        user: null,
+        setToken: (newToken: string) => set({ token: newToken }),
+        getSession: ():void => {
+          service.get("/users/session").then((res) => {
+            set({ user: res.data.user });
+          }).catch((err) => {
+            console.log(err)
+            set({ user: null });
+          })
+        },
+        isLogged: (): boolean => {
+          return !!useAuthStore.getState().user;
+        }
 }), {
   name: 'auth'
     }
