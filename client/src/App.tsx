ad3a8c6b0f3b031b24dd28f7cb38eb31e3b02bd2
CLOPEZPRO <khrystyan881@hotmail.com>
2023-07-07 16:37:26 -0500

    create anonimous function for get data user from server if cookies is correct and set data to store

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,19 @@
 import { Suspense, lazy } from 'react';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import { NavbarScreen } from './Components';
-
+import { useAuthStore } from "./store/auth";
 //add lazy loading to all pages
 const Home = lazy(() => import('./pages/Home/Home'));
 const Logout = lazy(() => import('./pages/Login/Login'));
 const Login = lazy(() => import('./pages/Login/Login'));
 const Register = lazy(() => import('./pages/Register/Register'));
 const AdminPanel = lazy(() => import('./pages/AdminPanel/AdminPanel'));
+
+
+(async function (){
+   useAuthStore.getState().getSession();
+})()
+
 
 const App = () => {
   return (
