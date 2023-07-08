1981ae772bda761c4c9281bfee2be65cd7e9801b
illiCristian <svrk73@gmail.com>
2023-07-07 23:12:52 -0300

    added zod validations for login and register

--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -1,10 +1,19 @@
 import { Router } from "express";
 import { authRequired } from "../middlewares/validateToken.js";
-import { register, login, logout, profile, listUsers, createUser, deleteUser, editUser } from "../controllers/admin.controller.js";
+import {
+  register,
+  login,
+  logout,
+  profile,
+  listUsers,
+  createUser,
+  deleteUser,
+  editUser,
+} from "../controllers/admin.controller.js";
 
 const adminRouter = Router();
 adminRouter.get("/", (req, res) => {
-    res.status(200).json({ message: "Welcome to feedback app, get users" });
+  res.status(200).json({ message: "Welcome to feedback app, get users" });
 });
 adminRouter.post("/register", register);
 adminRouter.post("/login", login);
@@ -14,11 +23,9 @@ adminRouter.get("/profile", profile);
 
 adminRouter.use(authRequired);
 
-
 adminRouter.get("/users", listUsers);
 adminRouter.post("/users", createUser);
 adminRouter.delete("/users/:id", deleteUser);
-adminRouter.put("/users/:id", editUser)
-
+adminRouter.put("/users/:id", editUser);
 
 export default adminRouter;
