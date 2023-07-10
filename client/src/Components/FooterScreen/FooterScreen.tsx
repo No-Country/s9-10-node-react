3c3154f169e77cfedeb8f6a1c890b663bc75657c
Diego-Fdez <dieghoxcr7@gmail.com>
2023-07-10 12:38:47 -0600

    add logo and links to footer

--- client/src/Components/FooterScreen/FooterScreen.tsx
+++ client/src/Components/FooterScreen/FooterScreen.tsx
@@ -1,13 +1,15 @@
-import { useScreenSize } from '../../hooks';
+import { useLocationState, useScreenSize } from '../../hooks';
 import { SocialMediaScreen, TabBottomScreen } from './components';
 
 function FooterScreen() {
   const { width } = useScreenSize();
+  const { isPathname } = useLocationState('/');
 
   return (
     <footer
-      className={`flex flex-col w-full justify-center items-center h-20 md:h-[91px] p-3 fixed 
-      md:static bottom-0 bg-slate-200`}
+      className={`flex flex-col w-full justify-center items-center h-20 ${
+        isPathname ? 'md:h-[288px]' : 'md:h-[91px]'
+      } p-3 fixed md:static bottom-0 bg-slate-200`}
     >
       {width < 768 ? <TabBottomScreen /> : <SocialMediaScreen />}
     </footer>
--- client/src/Components/FooterScreen/components/SocialMediaScreen/SocialMediaScreen.tsx
+++ client/src/Components/FooterScreen/components/SocialMediaScreen/SocialMediaScreen.tsx
@@ -4,10 +4,37 @@ import {
   LinkedinFilled,
 } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
+import { useLocationState } from '../../../../hooks';
 
 function SocialMediaScreen() {
+  const { isPathname } = useLocationState('/');
+
   return (
     <>
+      {isPathname && (
+        <div className='flex flex-row w-full h-52 items-center justify-between'>
+          <div className='flex flex-row gap-8 items-center justify-center ml-40'>
+            <div className='flex w-20 h-20 rounded-full bg-red-200'></div>
+            <h2 className=' text-[40px] font-bold text-black leading-[150%] tracking-tight'>
+              Logo
+            </h2>
+          </div>
+          <div className='flex flex-col w-72 mr-48'>
+            <Link
+              to='/'
+              className='text-[22px] font-semibold text-black leading-[150%] tracking-tight'
+            >
+              Términos y condiciones
+            </Link>
+            <Link
+              to='/'
+              className='text-[22px] font-semibold text-black leading-[150%] tracking-tight'
+            >
+              Política de Cookies
+            </Link>
+          </div>
+        </div>
+      )}
       <div className='flex flex-row w-44 h-11 justify-between items-center'>
         <Link to='https://www.facebook.com' target='_blank' rel='noreferrer'>
           <FacebookFilled style={{ color: '#1877F2', fontSize: '44px' }} />
