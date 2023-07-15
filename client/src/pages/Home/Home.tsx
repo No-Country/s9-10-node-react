e14459a6078f4870d5af2bb2fc71a2d1a3ab8c63
Diego-Fdez <dieghoxcr7@gmail.com>
2023-07-15 12:29:46 -0600

    add lazy loading

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,20 +1,33 @@
-import {
-  EndSectionScreen,
-  FunctionalitiesScreen,
-  HeadSectionScreen,
-  MiddleSectionScreen,
-  WhyUsSection,
-} from './components';
+import { lazy, Suspense } from 'react';
+import { Loader } from '../../Components';
+
+const EndSectionScreen = lazy(
+  () => import('./components/EndSection/EndSectionScreen')
+);
+const FunctionalitiesScreen = lazy(
+  () => import('./components/FunctionalitiesScreen/FunctionalitiesScreen')
+);
+const HeadSectionScreen = lazy(
+  () => import('./components/HeadSectionScreen/HeadSectionScreen')
+);
+const MiddleSectionScreen = lazy(
+  () => import('./components/MiddleSectionScreen/MiddleSectionScreen')
+);
+const WhyUsSection = lazy(
+  () => import('./components/WhyUsSection/WhyUsSection')
+);
 
 const Home = () => {
   return (
-    <main className='flex flex-col justify-center items-center w-full h-full'>
-      <HeadSectionScreen />
-      <FunctionalitiesScreen />
-      <MiddleSectionScreen />
-      <WhyUsSection />
-      <EndSectionScreen />
-    </main>
+    <Suspense fallback={<Loader />}>
+      <main className='flex flex-col justify-center items-center w-full h-full'>
+        <HeadSectionScreen />
+        <FunctionalitiesScreen />
+        <MiddleSectionScreen />
+        <WhyUsSection />
+        <EndSectionScreen />
+      </main>
+    </Suspense>
   );
 };
 
