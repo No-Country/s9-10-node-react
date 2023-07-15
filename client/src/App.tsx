e16892210fbce81add59e3a66897017d2e50f813
Diego-Fdez <dieghoxcr7@gmail.com>
2023-07-15 12:03:49 -0600

    add lazy loading

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,19 @@
 import { Suspense, lazy } from 'react';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import { FooterScreen } from './Components';
-import Root from './Components/Layout/Root';
 
 const Home = lazy(() => import('./pages/Home/Home'));
 const Logout = lazy(() => import('./pages/Login/Login'));
 const Login = lazy(() => import('./pages/Login/Login'));
 const Register = lazy(() => import('./pages/Register/Register'));
+const Root = lazy(() => import('./Components/Layout/Root'));
 const AdminPanel = lazy(() => import('./pages/AdminPanel/AdminPanel'));
 const RankingRol = lazy(() => import('./pages/RankingRol/RankingRol'));
 const RankingTeam = lazy(() => import('./pages/RankingTeam/RankingTeam'));
 const UserProfile = lazy(() => import('./pages/UserProfile/UserProfile'));
-const DashboardCompany = lazy(() => import('./pages/DashboardCompany/DashboardCompany'));
+const DashboardCompany = lazy(
+  () => import('./pages/DashboardCompany/DashboardCompany')
+);
 
 const App = () => {
   return (
@@ -25,9 +27,9 @@ const App = () => {
           <Route path='admin' element={<AdminPanel />} />
           <Route path='user' element={<Root />}>
             <Route index element={<DashboardCompany />} />
-            <Route path="ranking-rol" element={<RankingRol />} />
-            <Route path="ranking-equipos" element={<RankingTeam />} />
-            <Route path=":profileId" element={<UserProfile />} />
+            <Route path='ranking-rol' element={<RankingRol />} />
+            <Route path='ranking-equipos' element={<RankingTeam />} />
+            <Route path=':profileId' element={<UserProfile />} />
           </Route>
         </Routes>
         <FooterScreen />
