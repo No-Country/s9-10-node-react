1bf1ec14d66766a16fb46fd4a62a2bb49f9d8ce8
Diego-Fdez <dieghoxcr7@gmail.com>
2023-07-15 11:29:59 -0600

    update barrel

--- client/src/pages/Home/components/index.ts
+++ client/src/pages/Home/components/index.ts
@@ -1,3 +1,4 @@
 export { default as FunctionalitiesScreen } from './FunctionalitiesScreen/FunctionalitiesScreen';
 export { default as HeadSectionScreen } from './HeadSectionScreen/HeadSectionScreen';
 export { default as MiddleSectionScreen } from './MiddleSectionScreen/MiddleSectionScreen';
+export { default as WhyUsSection } from './WhyUsSection/WhyUsSection';
