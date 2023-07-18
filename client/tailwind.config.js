197c5f6b508e7eabc7ef76b05f34268bad894010
Henry Milac <henrymilacprog@gmail.com>
2023-07-18 00:29:00 -0500

    Navbar & Aside First advanced

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // navBackground: '#D7D7D7',
+        navBackground: '#ffffff',
+        textPrimary: '#08212B',
+        azulPrimary: '#185D81',
+        azulSecundary: '#2085B0',
+        azulClaroBg: '#D4EDF7',
+        azulClaroBg2: '#CCE8FF',
+        greyPrimary1: '#EAF0F5',
+        greyPrimary2: '#F4FAFF'
+      },
+    },
   },
   plugins: [],
 };
