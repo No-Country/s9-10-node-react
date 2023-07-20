a36832811b8bec919b732865fe0e2e966eaaca42
Diego-Fdez <dieghoxcr7@gmail.com>
2023-07-20 00:46:54 -0600

    add create forms page

--- client/src/pages/CreateForms/components/CustomButton/CustomButton.tsx
+++ client/src/pages/CreateForms/components/CustomButton/CustomButton.tsx
@@ -1,17 +1,32 @@
 interface CustomButtonProps {
+  isSelected?: number;
+  id?: number;
   label: string;
   isEnabled: boolean;
   handleClick?: () => void;
+  handleGetId: (id: number) => void;
 }
 
-function CustomButton({ label, isEnabled, handleClick }: CustomButtonProps) {
+function CustomButton({
+  isSelected = 0,
+  id = 1000,
+  label,
+  isEnabled,
+  handleClick,
+  handleGetId,
+}: CustomButtonProps) {
   return (
     <button
-      className={`flex items-center justify-center w-[21rem] h-10 rounded-[0.625rem] bg-[#E9ECFC] text-black text-xl font-normal
+      className={`flex items-center justify-center w-[21rem] h-10 rounded-[0.625rem] text-black text-xl font-normal
           leading-7 tracking-[-0.0275rem] hover:bg-[#7A8CEB] transition-colors duration-300 ${
             !isEnabled && 'disabled:bg-slate-400'
-          } md:w-[13rem] md:h-14 md:text-base md:leading-6 md:tracking-[-0.022rem]`}
-      onClick={handleClick}
+          } md:w-[13rem] md:h-14 md:text-base md:leading-6 md:tracking-[-0.022rem] ${
+        isSelected === id ? 'bg-[#7A8CEB]' : 'bg-[#E9ECFC]'
+      }`}
+      onClick={() => {
+        handleClick();
+        handleGetId(id);
+      }}
       disabled={!isEnabled}
     >
       {label}
