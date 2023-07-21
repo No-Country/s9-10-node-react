53d11c1866ca3169e3728bdc48616ba344aaf86f
Diego-Fdez <dieghoxcr7@gmail.com>
2023-07-20 18:48:30 -0600

    update barrel

--- client/src/pages/CreateForms/components/index.ts
+++ client/src/pages/CreateForms/components/index.ts
@@ -1,4 +1,9 @@
 export { default as CustomButton } from './CustomButton/CustomButton';
+export { default as EmptyQuestionScreen } from './EmptyQuestionScreen/EmptyQuestionScreen';
 export { default as MenuButtons } from './MenuButtons/MenuButtons';
+export { default as OneOptionScreen } from './OneOptionScreen/OneOptionScreen';
+export { default as OpenEndedQuestion } from './Open-EndedQuestion/OpenEndedQuestion';
 export { default as OptionsButtons } from './OptionsButtons/OptionsButtons';
+export { default as QuestionsInput } from './QuestionsInput/QuestionsInput';
+export { default as SaveButton } from './SaveButton/SaveButton';
 export { default as TitleScreen } from './TitleScreen/TitleScreen';
---
+++ client/src/pages/CreateForms/models/index.ts
@@ -0,0 +1,1 @@
+export * from './CreateForms.types';
