6e4e58ce72c51a452bef63abffc2a14410f0cbb4
Diego-Fdez <dieghoxcr7@gmail.com>
2023-07-21 10:56:20 -0600

    fix styles

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -34,16 +34,16 @@ const Login = () => {
   return (
     <>
       <HeadScreen title='Inicio de Sesión' />
-      <div className='flex flex-col justify-center min-h-screen py-12 bg-gray-100 sm:px-6 lg:px-8'>
+      <div className='flex flex-col justify-center min-h-screen py-12 bg-white sm:px-6 lg:px-8'>
         <div className='sm:mx-auto sm:w-full sm:max-w-md'>
-          <h2 className='mt-6 text-3xl font-extrabold text-center text-gray-900'>
+          <h2 className='mt-6 text-3xl font-extrabold text-center text-black'>
             Inicia sesión en tu cuenta
           </h2>
           <p className='mt-2 text-sm text-center text-gray-600 max-w'>
             O{' '}
             <a
               href='#'
-              className='font-medium text-blue-600 hover:text-blue-500'
+              className='font-medium text-[#7A8CEB] hover:text-blue-500'
               onClick={() => navigate('/register')}
             >
               Crea una cuenta
@@ -56,7 +56,7 @@ const Login = () => {
             <form className='space-y-6' action='#' onSubmit={handleSubmit}>
               <div>
                 <label className='block text-sm font-medium text-gray-700'>
-                  Email
+                  Correo electrónico
                 </label>
                 <div className='mt-1'>
                   <input
@@ -64,7 +64,8 @@ const Login = () => {
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                     required
-                    className='relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+                    className={`relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md 
+                    appearance-none focus:outline-none focus:ring-[#CCE8FF] focus:border-[#CCE8FF] focus:z-10 sm:text-sm`}
                     placeholder='Ingresa tu email'
                   ></input>
                 </div>
@@ -80,7 +81,8 @@ const Login = () => {
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     required
-                    className='relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+                    className={`relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md 
+                    appearance-none focus:outline-none focus:ring-[#CCE8FF] focus:border-[#CCE8FF] focus:z-10 sm:text-sm`}
                     placeholder='Ingresa tu contraseña'
                   ></input>
                 </div>
@@ -103,7 +105,7 @@ const Login = () => {
                 <div className='text-sm'>
                   <a
                     href='#'
-                    className='font-medium text-blue-600 hover:text-blue-500'
+                    className='font-medium text-[#7A8CEB] hover:text-blue-500'
                   >
                     Olvidaste tu contraseña?
                   </a>
@@ -113,63 +115,14 @@ const Login = () => {
               <div>
                 <button
                   type='submit'
-                  className='relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md group hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                  className={`relative flex justify-center w-full px-4 py-2 text-sm font-medium text-[#CCE8FF] bg-[#185D81] 
+                  rounded-md group hover:bg-[#CCE8FF] hover:text-[#185D81] transition-colors duration-300`}
                 >
                   Iniciar sesión
                 </button>
               </div>
             </form>
-            <div className='mt-6'>
-              <div className='relative'>
-                <div className='absolute inset-0 flex items-center'>
-                  <div className='w-full border-t border-gray-300'></div>
-                </div>
-                <div className='relative flex justify-center text-sm'>
-                  <span className='px-2 text-gray-500 bg-gray-100'>
-                    O continua con
-                  </span>
-                </div>
-              </div>
-
-              <div className='grid grid-cols-3 gap-3 mt-6'>
-                <div>
-                  <a
-                    href='#'
-                    className='flex items-center justify-center w-full px-8 py-3 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50'
-                  >
-                    <img
-                      className='w-5 h-5'
-                      src='https://www.svgrepo.com/show/512120/facebook-176.svg'
-                      alt=''
-                    />
-                  </a>
-                </div>
-                <div>
-                  <a
-                    href='#'
-                    className='flex items-center justify-center w-full px-8 py-3 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50'
-                  >
-                    <img
-                      className='w-5 h-5'
-                      src='https://www.svgrepo.com/show/513008/twitter-154.svg'
-                      alt=''
-                    />
-                  </a>
-                </div>
-                <div>
-                  <a
-                    href='#'
-                    className='flex items-center justify-center w-full px-8 py-3 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50'
-                  >
-                    <img
-                      className='w-6 h-6'
-                      src='https://www.svgrepo.com/show/506498/google.svg'
-                      alt=''
-                    />
-                  </a>
-                </div>
-              </div>
-            </div>
+            <div className='mt-6'></div>
           </div>
         </div>
       </div>
