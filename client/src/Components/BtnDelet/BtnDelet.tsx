3a6d322a33982857cf0ae96ac698137907f2affe
Diego-Fdez <dieghoxcr7@gmail.com>
2023-07-23 19:29:30 -0600

    add modal

--- client/src/Components/BtnDelet/BtnDelet.tsx
+++ client/src/Components/BtnDelet/BtnDelet.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
-import { Modal } from "antd";
-import { DeleteOutlined } from "@ant-design/icons";
-import Objetos from "./OBJECTS.jpg";
-import "./BtnDelet.css";
+import React, { useState } from 'react';
+import { Modal } from 'antd';
+import { DeleteOutlined } from '@ant-design/icons';
+import Objetos from './OBJECTS.jpg';
+import './BtnDelet.css';
+import { ModalAlert } from '..';
 
 interface BtnDeletProps {
   onDelete: () => void;
@@ -15,79 +16,78 @@ const BtnDelet: React.FC<BtnDeletProps> = ({ onDelete }) => {
     setIsModalOpen(true);
   };
 
-  const handleOk = () => {
-    setIsModalOpen(false);
-    onDelete(); // Llama a la función onDelete cuando se confirme el modal.
-  };
+  // const handleOk = () => {
+  //   setIsModalOpen(false);
+  //   onDelete(); // Llama a la función onDelete cuando se confirme el modal.
+  // };
 
-  const handleCancel = () => {
-    setIsModalOpen(false);
-  };
+  // const handleCancel = () => {
+  //   setIsModalOpen(false);
+  // };
 
   return (
     <>
       {/* Botón para abrir el modal */}
-      <div className="delet-cont" onClick={showModal}>
-        <div className="delet-icon">
+      <div className='delet-cont' onClick={showModal}>
+        <div className='delet-icon'>
           <DeleteOutlined />
         </div>
-        <div className="delet-eliminar">Eliminar</div>
+        <div className='delet-eliminar'>Eliminar</div>
       </div>
 
       {/* Modal personalizado */}
 
-      <Modal
-        title={
-          <p className="custom-modal-title">
-            ¿Seguro que deseas eliminar el equipo?
-          </p>
-        }
-        visible={isModalOpen}
+      {/* <Modal
+        title={<p className='custom-modal-title'>{title}</p>}
+        open={isModalOpen}
         onCancel={handleCancel}
         width={672}
         footer={[
           <>
-            <div className="cont-btn">
-            <div
-                key="cancel-button"
+            <div className='cont-btn'>
+              <div
+                key='cancel-button'
                 onClick={handleCancel}
-                className="modal-cancel-button"
+                className='modal-cancel-button'
               >
                 Cancelar
               </div>
               <div
-                key="ok-button"
+                key='ok-button'
                 onClick={handleOk}
-                className="modal-ok-button"
+                className='modal-ok-button'
               >
                 <DeleteOutlined />
                 Eliminar equipo
               </div>
               ,
-              
             </div>
           </>,
         ]}
       >
-        <div className="modal-cont">
-          <div className="modal-image">
+        <div className='modal-cont'>
+          <div className='modal-image'>
             <img
               src={Objetos}
-              alt="Objetos"
-              style={{ width: "208px", height: "157px", marginTop: "25%" }}
+              alt='Objetos'
+              style={{ width: '208px', height: '157px', marginTop: '25%' }}
             />
           </div>
-          <div className="modal-text">
+          <div className='modal-text'>
             <p>
-              Al eliminar el equipo ya no se podrán visualizar los usuarios
-              pertenecientes.
-              <span style={{ fontWeight: "700" }}>
+              {content}
+              <span style={{ fontWeight: '700' }}>
                 No se podrá deshacer esta acción.
               </span>
             </p>
           </div>
         </div>
-      </Modal>
+      </Modal> */}
+      <ModalAlert
+        onDelete={onDelete}
+        isModalOpen={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+      />
     </>
   );
 };
---
+++ client/src/Components/BtnDelet/ModalAlert.tsx
@@ -0,0 +1,76 @@
+import { DeleteOutlined } from '@ant-design/icons';
+import { Modal } from 'antd';
+import Objetos from './OBJECTS.jpg';
+
+interface ModalProps {
+  onDelete: () => void;
+  title?: string;
+  content?: string;
+  isModalOpen: boolean;
+  setIsModalOpen: (value: boolean) => void;
+}
+
+function ModalAlert({
+  title = '¿Seguro que deseas eliminar el equipo?',
+  content = `Al eliminar el equipo ya no se podrán visualizar los usuarios
+pertenecientes.`,
+  onDelete,
+  isModalOpen,
+  setIsModalOpen,
+}: ModalProps) {
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+    onDelete(); // Llama a la función onDelete cuando se confirme el modal.
+  };
+
+  return (
+    <Modal
+      title={<p className='custom-modal-title'>{title}</p>}
+      open={isModalOpen}
+      onCancel={handleCancel}
+      width={672}
+      footer={[
+        <>
+          <div className='cont-btn'>
+            <div
+              key='cancel-button'
+              onClick={handleCancel}
+              className='modal-cancel-button'
+            >
+              Cancelar
+            </div>
+            <div key='ok-button' onClick={handleOk} className='modal-ok-button'>
+              <DeleteOutlined />
+              Eliminar equipo
+            </div>
+            ,
+          </div>
+        </>,
+      ]}
+    >
+      <div className='modal-cont'>
+        <div className='modal-image'>
+          <img
+            src={Objetos}
+            alt='Objetos'
+            style={{ width: '208px', height: '157px', marginTop: '25%' }}
+          />
+        </div>
+        <div className='modal-text'>
+          <p>
+            {content}
+            <span style={{ fontWeight: '700' }}>
+              No se podrá deshacer esta acción.
+            </span>
+          </p>
+        </div>
+      </div>
+    </Modal>
+  );
+}
+
+export default ModalAlert;
