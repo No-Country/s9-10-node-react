d2c8cb7a225c15c692bb1e3e41ad8ae3b74413fa
Diego-Fdez <dieghoxcr7@gmail.com>
2023-07-23 19:29:14 -0600

    update barrel

--- client/src/Components/index.ts
+++ client/src/Components/index.ts
@@ -3,3 +3,5 @@ export { default as NavbarScreen } from './NavbarScreen/NavbarScreen';
 export { default as SearchBoxScreen } from './SearchBoxScreen/SearchBoxScreen';
 export { default as Loader } from './Loader/Loader';
 export { default as HeadScreen } from './HeadScreen/HeadScreen';
+export { default as BtnDelet } from './BtnDelet/BtnDelet';
+export { default as ModalAlert } from './BtnDelet/ModalAlert';
