ceba14b059409579c9e90b0779c3a2acd6061482
Diego Panno <diegompanno@gmail.com>
2023-07-25 19:21:16 -0300

    Aside colapsado

--- client/src/Components/Layout/Root.tsx
+++ client/src/Components/Layout/Root.tsx
@@ -1,16 +1,20 @@
 import { Outlet } from 'react-router-dom';
 import NavbarScreen from '../NavbarScreen/NavbarScreen';
-import Aside from '../Aside/Aside';
 import Toolbar from '../Toolbar/Toolbar';
+import AsidePadre from '../Aside/AsidePadre';
 
 const Root = () => {
+  const handleLogout = () => {
+    // Lógica para cerrar sesión
+    // Por ejemplo, eliminar el token de sesión o realizar una llamada a la API para cerrar sesión
+  };
   return (
     <div>
       <NavbarScreen />
 
       <div className="md:flex mb-20 md:mb-0">
         <div className="hidden md:block">
-          <Aside />
+          <AsidePadre handleLogout={handleLogout} />
         </div>
         <main className="w-full">
           <Outlet />
