a9245e5294b03cceefef35dcd30606dc43ed4b1d
Diego-Fdez <dieghoxcr7@gmail.com>
2023-07-26 21:40:05 -0600

    disable page

--- client/src/pages/Logout/Logout.tsx
+++ client/src/pages/Logout/Logout.tsx
@@ -1,27 +1,26 @@
-import { useEffect } from 'react';
-import { logoutRequest } from "../../api/auth";
+// import { useEffect } from 'react';
+// import { logoutRequest } from "../../api/auth";
 
-const Logout = () => {
-  useEffect(() => {
-    const logout = async () => {
-      try {
-        const res = await logoutRequest();
-        console.log(res); // Manejar la respuesta según tus necesidades
-      } catch (error) {
-        console.log(error); // Manejar el error según tus necesidades
-      }
-    };
+// const Logout = () => {
+//   useEffect(() => {
+//     const logout = async () => {
+//       try {
+//         const res = await logoutRequest();
+//         console.log(res); // Manejar la respuesta según tus necesidades
+//       } catch (error) {
+//         console.log(error); // Manejar el error según tus necesidades
+//       }
+//     };
 
-    logout();
-  }, []);
+//     logout();
+//   }, []);
 
-  return (
-    
-    <div>
-      <h2>Logging out...</h2>
-    </div>
-  );
-};
+//   return (
 
-export default Logout;
+//     <div>
+//       <h2>Logging out...</h2>
+//     </div>
+//   );
+// };
 
+// export default Logout;
